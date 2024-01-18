567784d467beb879e008891a42687d59e647f8ee
Jason Hendriks <jason@hendriks.ca>
2024-01-18 15:24:39 -0500

    add Thymeleaf library to render web pages

---
+++ web/src/main/java/ca/hendriks/bartender/web/HomeRestController.java
@@ -0,0 +1,20 @@
+package ca.hendriks.bartender.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeRestController {
+
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(final Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+
+}
