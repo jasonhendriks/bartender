185fb0e0fd1d5ed13754c66513585ece615423a2
Marie M <29389592+MarieIndustries@users.noreply.github.com>
2024-01-21 00:29:51 -0500

    adding recipe step for the project to install successfully

--- web/src/integration-test/java/ca/hendriks/bartender/web/functionaltest/recipe/RecipeSteps.java
+++ web/src/integration-test/java/ca/hendriks/bartender/web/functionaltest/recipe/RecipeSteps.java
@@ -19,6 +19,16 @@ public class RecipeSteps {
         dsl.recipes.addRecipe(recipe);
     }
 
+    @When("an administrator adds ingredients for a recipe named {string} with method {string}:")
+    public void an_administrator_adds_ingredients_for_a_recipe_named_with_method(String recipeName, String recipeMethod, final DataTable dataTable) {
+//        final Recipe recipe = new RecipeBuilder()
+//                .name(recipeName)
+//                .method(recipeMethod)
+//                .build();
+//        dsl.recipes.addRecipe(recipe);
+    }
+
+
     @Then("there is {int} recipe in the system.")
     public void there_is_recipe_in_the_system(Integer int1) {
     }
